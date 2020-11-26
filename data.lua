54599c0c15d9e94e188d59f048bf47d6f437a367
Distortions81 <carlotto81@gmail.com>
2020-11-26 09:29:37 -0700

    update for Factorio 1.1

---
+++ data.lua
@@ -0,0 +1,10 @@
+data:extend({
+	{
+     type = "sprite",
+     name = "CleanMap_button",
+     filename = "__CleanMap__/graphics/CleanMap_button.png",
+     priority = "extra-high-no-scale",
+     width = 32,
+     height = 32
+	},
+})
---
+++ settings.lua
@@ -0,0 +1,17 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "CleanMap_radius",
+		setting_type = "runtime-global",
+		minimum_value = 0,
+		default_value = 2,
+		order = "2"
+	},
+	{
+		type = "bool-setting",
+		name = "CleanMap_paving",
+		setting_type = "runtime-global",
+		default_value = true,
+		order = "3"
+	}
+})
