083eb981bca2c80520a9a1655c9016a5a87860fe
Distortions81 <carlotto81@gmail.com>
2021-03-17 23:17:54 -0600

    skip spaceships for se

---
+++ settings.lua
@@ -0,0 +1,31 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "CleanMap_radius",
+		setting_type = "runtime-global",
+		minimum_value = 0,
+		default_value = 2,
+		order = "2"
+	},
+	{
+		type = "bool-setting",
+		name = "CleanMap_paving",
+		setting_type = "runtime-global",
+		default_value = true,
+		order = "3"
+	},
+	{
+		type = "bool-setting",
+		name = "CleanMap_replay",
+		setting_type = "runtime-global",
+		default_value = true,
+		order = "4"
+	},
+	{
+		type = "bool-setting",
+		name = "CleanMap_radar",
+		setting_type = "runtime-global",
+		default_value = false,
+		order = "5"
+	}
+})
